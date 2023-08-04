8c65ae15948040d02ad688c3e22a3c90b0c5e79e
Mocky <arnaud.solier@outlook.fr>
2023-08-04 12:21:56 +0200

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'city_card.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('Dyma Trip'),
+        actions: const <Widget>[Icon(Icons.more_vert)],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            CityCard(image: 'images/paris.jpg', name: 'Paris'),
+          ],
+        ),
+      ),
+    );
+  }
+}
